df52aa6a302b54733b12c20659d59945871a8de5
jorturfer <jorge_turrado@hotmail.es>
2021-07-04 16:01:10 +0200

    Adds missing files

---
+++ UNIT3D-Helper/Entities/PrometheusData.cs
@@ -0,0 +1,9 @@
+﻿namespace UNIT3D_Helper.Entities
+{
+    public class PrometheusData
+    {
+        public int Thanks { get; set; }
+        public int Comments { get; set; }
+        public int Tips { get; set; }
+    }
+}
